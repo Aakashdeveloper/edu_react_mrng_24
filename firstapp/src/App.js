a556fe5b07826689b89071e815e8f95669f4ab51
aakashdeveloper <ahanda205@gmail.com>
2023-12-03 09:52:13 +0530

    component

---
+++ firstapp/src/App.js
@@ -0,0 +1,16 @@
+import React,{Fragment} from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const App = () => {
+    return(
+        <Fragment>
+            <Header/>
+            <h2>Home Page</h2>
+            <Footer/>
+        </Fragment>
+       
+    )
+}
+
+export default App;
---
+++ firstapp/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <React.Fragment>
+            <hr/>
+            <center>
+                <h3>Developer Funnel &copy; 2024</h3>
+            </center>
+        </React.Fragment>
+    )
+}
+
+export default Footer;
